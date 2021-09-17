ebd884848bfc92bf284da382eddde28bce71eee7
czylabsonasa <czylabson.asa@gmail.com>
2021-09-17 08:55:09 +0200

    progress

--- imag/main2e.jl
+++ imag/main2e.jl
@@ -1,4 +1,4 @@
-# grayscale play
+# rgb play
 
 using 
   Gtk, 
@@ -33,15 +33,28 @@ cx,cy=200,200
 can = GtkCanvas(cx,cy)
 set_gtk_property!(can, :expand, true)
 
-RGBslid, RGBslidval = mkslid(0:255, UInt8(125))
+Rslid, Rslidval = mkslid(0:255, UInt8(125))
+Gslid, Gslidval = mkslid(0:255, UInt8(125))
+Bslid, Bslidval = mkslid(0:255, UInt8(125))
 
-on(RGBslidval) do val
-  draw(can)
-end
+for slidval in [Rslidval, Gslidval, Bslidval]
+  on(slidval) do val
+    #println("changed to $(val)...")
+    draw(can)
+  end
+end  
+
+#println(typeof(slidval), " ", slidval[])
+
+#im = fill(argb(slidval[],120,0,0),cx, cy)
+
+
 
 
 gr = GtkGrid()
-gr[1,2] = GtkLabel("RGB"); gr[2,2] = RGBslid
+gr[2,2] = Rslid; gr[1,2] = GtkLabel("R")
+gr[2,3] = Gslid; gr[1,3] = GtkLabel("G")
+gr[2,4] = Bslid; gr[1,4] = GtkLabel("B")
 gr[1:2,5:6] = can
 
 set_gtk_property!(gr, :expand, true)
@@ -50,7 +63,7 @@ set_gtk_property!(gr, :expand, true)
 can.draw = function(_)
   ctx = getgc(can)
   rectangle(ctx, 0, 0, width(can), height(can))
-  set_source_rgb(ctx, RGBslidval[]/255, RGBslidval[]/255, RGBslidval[]/255)
+  set_source_rgb(ctx, Rslidval[]/255, Gslidval[]/255, Bslidval[]/255)
   fill(ctx)
 end
 
